448438b904cf645d17693875c4813b9ac2044210
Ricardo Gomez <ricardo.gomez331@gmail.com>
2012-06-04 19:56:53 -0500

    Typo in cm.mk

--- cm.mk
+++ cm.mk
@@ -3,20 +3,20 @@
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/intercept/full_intercpt.mk)
+$(call inherit-product, device/samsung/intercept/full_intercept.mk)
 
 # Inherit CDMA definitions
 $(call inherit-product, vendor/cm/config/cdma.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_intercpt
+PRODUCT_NAME := cm_intercept
 PRODUCT_BRAND := samsung
-PRODUCT_DEVICE := intercpt
+PRODUCT_DEVICE := intercept
 PRODUCT_MODEL := SPH-M910
 PRODUCT_MANUFACTURER := samsung
 
 # Release name and versioning
-PRODUCT_RELEASE_NAME := Transform
+PRODUCT_RELEASE_NAME := Intercept
 PRODUCT_VERSION_DEVICE_SPECIFIC :=Samsung_intercept_CM9_alpha1
 
 -include vendor/cm/products/common_versions.mk
